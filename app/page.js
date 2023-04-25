4fd846cd013edac317e80ccb0b3e3f6de335b3af
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-25 17:41:56 -0400

    save

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
     <main className={styles['flex-row']}>
       <div className={styles['left-text']}>
         <h1 className={styles['title']}>The Forge</h1>
-        <p className={styles['description']}>a prompt optimizer based on <a className={styles['link']} href="https://www.paperswithprompts.com/">research papers</a></p>
+        <p className={styles['description']}>a prompt optimizer based on <a className={styles['link']} href="https://www.paperswithprompts.com/">research&nbsp;papers</a></p>
+        <button className={styles['button']}>Enter Prompt</button>
       </div>
       <Image className={styles['art']} src="/art.jpeg" alt="Vercel Logo" width={2048 / 6} height={3072 / 6} />
     </main>
