fca12d31d30140e778343d2e6b3a0da955049e10
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-25 16:08:34 -0400

    first commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles['flex-row']}>
+      <div className={styles['left-text']}>
+        <h1 className={styles['title']}>The Forge</h1>
+        <p className={styles['description']}>a prompt optimizer based on <a className={styles['link']} href="https://www.paperswithprompts.com/">research papers</a></p>
+      </div>
+      <Image className={styles['art']} src="/art.jpeg" alt="Vercel Logo" width={2048 / 6} height={3072 / 6} />
+    </main>
+  )
+}
