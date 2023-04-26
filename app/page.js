4d07202569b10be7a4056ab408e310a27de5cfdf
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-25 20:03:32 -0400

    styles on mobile fixed

--- app/page.js
+++ app/page.js
@@ -1,15 +1,24 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main className={styles['flex-row']}>
-      <div className={styles['left-text']}>
-        <h1 className={styles['title']}>The Forge</h1>
-        <p className={styles['description']}>a prompt optimizer based on <a className={styles['link']} href="https://www.paperswithprompts.com/">research&nbsp;papers</a></p>
-        <button className={styles['button']}>Enter Prompt</button>
-      </div>
-      <Image className={styles['art']} src="/art.jpeg" alt="Vercel Logo" width={2048 / 6} height={3072 / 6} />
-    </main>
+    <>
+      <Head >
+        <title>Forge</title>
+        <meta name="description" content="A Prompt Optimizer based on Research Papers" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <main className={styles['row']}>
+        <div className={styles['left-text']}>
+          <h1 className={styles['title']}>The Forge</h1>
+          <p className={styles['description']}>a prompt optimizer based on <a className={styles['link']} href="https://www.paperswithprompts.com/">research&nbsp;papers</a></p>
+          <button className={styles['button']}>Enter Prompt</button>
+        </div>
+        <Image className={styles['art']} src="/art.jpeg" alt="Vercel Logo" width={2048 / 6} height={3072 / 6} />
+      </main>
+    </>
   )
 }
